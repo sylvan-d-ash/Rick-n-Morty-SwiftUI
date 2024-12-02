6253b4c2bf0aa6c44d787244f191408a6f1ccb27
Sylvan Ash <sylvan.d.ash@gmail.com>
2024-12-02 16:48:26 +0300

    Add Character model

---
+++ Rick n Morty Characters/Character.swift
@@ -0,0 +1,35 @@
+//
+//  Character.swift
+//  Rick n Morty Characters
+//
+//  Created by Sylvan Ash on 02/12/2024.
+//
+
+import Foundation
+
+struct Character: Decodable, Identifiable {
+    enum Gender: String, Decodable {
+        case female = "Female"
+        case male = "Male"
+        case genderless = "Genderless"
+        case unknown = "unknown"
+    }
+
+    enum Status: String, Decodable {
+        case alive = "Alive"
+        case dead = "Dead"
+        case unknown = "Unknown"
+    }
+
+    struct Location: Decodable {
+        let name: String
+    }
+
+    let id: Int
+    let name: String
+    let species: String
+    let image: String
+    let status: Status
+    let gender: Gender
+    let location: Location
+}
