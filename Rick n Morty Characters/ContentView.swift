209ef82e0764b623bd017200ae6734fd6466bb3c
Sylvan Ash <sylvan.d.ash@gmail.com>
2024-12-02 16:43:33 +0300

    Initial Commit

---
+++ Rick n Morty Characters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rick n Morty Characters
+//
+//  Created by Sylvan Ash on 02/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rick n Morty Characters/Rick_n_Morty_CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_n_Morty_CharactersApp.swift
+//  Rick n Morty Characters
+//
+//  Created by Sylvan Ash on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rick_n_Morty_CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
