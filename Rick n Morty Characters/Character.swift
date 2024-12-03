6e68120528accd3648b7d20c98ca2e0852a28f6c
Sylvan Ash <sylvan.d.ash@gmail.com>
2024-12-04 00:30:30 +0300

    Fix decoding issues

--- Rick n Morty Characters/Character.swift
+++ Rick n Morty Characters/Character.swift
@@ -31,5 +31,28 @@ struct Character: Decodable, Identifiable {
     let image: String
     let status: Status
     let gender: Gender
-    let location: Location
+    let location: String
+}
+
+extension Character {
+    private enum CodingKeys: String, CodingKey {
+        case id, name, species, image, status, gender, location
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        species = try container.decode(String.self, forKey: .species)
+        image = try container.decode(String.self, forKey: .image)
+
+        let _status = try container.decode(String.self, forKey: .status)
+        status = Status(rawValue: _status) ?? .unknown
+
+        let _gender = try container.decode(String.self, forKey: .gender)
+        gender = Gender(rawValue: _gender) ?? .unknown
+
+        let _location = try container.decode(Location.self, forKey: .location)
+        location = _location.name
+    }
 }
