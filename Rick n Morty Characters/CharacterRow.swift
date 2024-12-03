c6e4792fb1830fad25207394a9fb9d6b98491d10
Sylvan Ash <sylvan.d.ash@gmail.com>
2024-12-04 00:34:43 +0300

    Update CharacterRow to use values from the charater item

--- Rick n Morty Characters/CharacterRow.swift
+++ Rick n Morty Characters/CharacterRow.swift
@@ -8,19 +8,27 @@
 import SwiftUI
 
 struct CharacterRow: View {
+    var character: Character
+
     var body: some View {
         HStack(alignment: .top, spacing: 16) {
-            Image("toxic_rick", bundle: nil)
-                .resizable()
-                .frame(width: 100, height: 100)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
+            AsyncImage(url: URL(string: character.image)) { image in
+                image
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+
+            } placeholder: {
+                ProgressView()
+                    .frame(width: 100, height: 100)
+            }
 
             VStack(alignment: .leading, spacing: 4) {
-                Text("Zephyr")
+                Text(character.name)
                     .font(.system(size: 24, weight: .bold))
                     .foregroundStyle(.primary)
 
-                Text("Elf")
+                Text(character.species)
                     .font(.system(size: 20))
                     .foregroundStyle(.secondary)
             }
@@ -37,5 +45,14 @@ struct CharacterRow: View {
 }
 
 #Preview {
-    CharacterRow()
+    let character = Character(id: 1,
+                              name: "Toxic Rick",
+                              species: "Humanoid",
+                              image: "https://rickandmortyapi.com/api/character/avatar/361.jpeg",
+                              status: .dead,
+                              gender: .male,
+                              location: "Earth"
+    )
+
+    return CharacterRow(character: character)
 }
