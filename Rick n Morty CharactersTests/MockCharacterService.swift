f2771ae4151c51a0ea5a431660e7567676a49192
Sylvan Ash <sylvan.d.ash@gmail.com>
2024-12-04 17:23:28 +0300

    A bit of tests cleanup

---
+++ Rick n Morty CharactersTests/MockCharacterService.swift
@@ -0,0 +1,19 @@
+//
+//  MockCharacterService.swift
+//  Rick n Morty CharactersTests
+//
+//  Created by Sylvan Ash on 04/12/2024.
+//
+
+import Foundation
+@testable import Rick_n_Morty_Characters
+
+final class MockCharacterService: CharacterService {
+    var fetchCharactersResult: Result<CharactersResponse, Error>?
+    private(set) var page: Int?
+
+    func fetchCharacters(page: Int) async -> Result<CharactersResponse, Error> {
+        self.page = page
+        return fetchCharactersResult!
+    }
+}
